12829632b5922c0238de37081bf6c914d299a719
Kundan Kumar <93861196+its-kundan@users.noreply.github.com>
2024-09-02 17:35:00 +0530

    Time: 4 ms (84.5%), Space: 26.2 MB (43.14%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -7,9 +7,7 @@ class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         for (int i = 0; i < k; ++i) {
-            // Find the index of the first minimum value in the array
             int minIndex = distance(nums.begin(), min_element(nums.begin(), nums.end()));
-            // Multiply the element at minIndex by the multiplier
             nums[minIndex] *= multiplier;
         }
         return nums;
