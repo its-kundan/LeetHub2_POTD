190c51cad643fa3cb4f21608def2190e90bd7c27
Kundan Kumar <93861196+its-kundan@users.noreply.github.com>
2024-12-17 12:52:55 +0530

    Time: 0 ms (100%), Space: 27.8 MB (20.8%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    struct compare {
-        bool operator()(pair<int, int> a, pair<int, int> b) {
-            if (a.first == b.first) {
-                return a.second > b.second;
-            }
-            return a.first > b.first;
-        }
-    };
-
-    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
-        for (int i = 0; i < nums.size(); i++) {
+    vector<int> getFinalState(vector<int>& nums, int k, int m) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(int i = 0; i < nums.size(); i++) {
             pq.push({nums[i], i});
         }
-        while (k--) {
-            auto Y = pq.top();
+        while(k--) {
+            auto it = pq.top();
             pq.pop();
-            pq.push({Y.first * multiplier, Y.second});
-        }
-        while (!pq.empty()) {
-            auto to = pq.top();
-            pq.pop();
-            nums[to.second] = to.first;
+            nums[it.second] = m * it.first;
+            pq.push({m * it.first, it.second}); 
         }
         return nums;
     }
